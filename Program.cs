26cf32a56f48a70a55f9ea9e733f8c5d43a9a0f8
Pp-4 <39858427+Pp-4@users.noreply.github.com>
2024-03-01 14:09:20 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TickTackToeNetSocket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
